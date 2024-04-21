0b35235c7c7939a096e6e91d525ff86b0bb111f0
Nitin-Anil-Narang <narangnitin520@gmail.com>
2024-04-22 02:43:21 +0530

    modified the button and divisons

---
+++ assets/js/button.js
@@ -0,0 +1,29 @@
+// $(function(){
+//     $('#button').click(() => { 
+//         if(!$('#iframe').length) {
+// 			//var replaced = $('#iframeHolder').html().replace('<iframe id = "#myiFrame" width="100%" height="800" frameborder="0" allow="xr-spatial-tracking; gyroscope; accelerometer" allowfullscreen scrolling="no" src="https://kuula.co/share/collection/7XlH7?logo=1&info=0&fs=1&vr=1&zoom=1&initload=0&autorotate=0.1&thumbs=1"></iframe>,<iframe id = "#myiFrame" width="100%" height="1000" frameborder="0" allow="xr-spatial-tracking; gyroscope; accelerometer" allowfullscreen scrolling="no" src="https://kuula.co/share/collection/7XlH7?logo=1&info=0&fs=1&vr=1&zoom=1&initload=0&autorotate=0.1&thumbs=1"></iframe>'
+//             $('#iframeHolder').remove();
+// 			$('#iframeHolder').replaceWith('<iframe id = "#myiFrame" width="100%" height="800" frameborder="0" allow="xr-spatial-tracking; gyroscope; accelerometer" allowfullscreen scrolling="no" src="https://kuula.co/share/collection/7XlH7?logo=1&info=0&fs=1&vr=1&zoom=1&initload=0&autorotate=0.1&thumbs=1"></iframe>');
+// 		}
+//     });   
+// })
+
+// $(function(){
+//     $('#button2').click(function(){ 
+//         if(!$('#iframe').length) {
+// 			//var replaced = $('#iframeHolder').html().replace('<iframe id = "#myiFrame" width="100%" height="800" frameborder="0" allow="xr-spatial-tracking; gyroscope; accelerometer" allowfullscreen scrolling="no" src="https://kuula.co/share/collection/7XlH7?logo=1&info=0&fs=1&vr=1&zoom=1&initload=0&autorotate=0.1&thumbs=1"></iframe>,<iframe id = "#myiFrame" width="100%" height="1000" frameborder="0" allow="xr-spatial-tracking; gyroscope; accelerometer" allowfullscreen scrolling="no" src="https://kuula.co/share/collection/7XlH7?logo=1&info=0&fs=1&vr=1&zoom=1&initload=0&autorotate=0.1&thumbs=1"></iframe>')
+// 			$('#iframeHolder').remove();
+// 			$('#iframeHolder').replaceWith('<iframe id = "#myiFrame" width="100%" height="800" frameborder="0" allow="xr-spatial-tracking; gyroscope; accelerometer" allowfullscreen scrolling="no" src="https://kuula.co/share/collection/7XlH7?logo=1&info=0&fs=1&vr=1&zoom=1&initload=0&autorotate=0.1&thumbs=1"></iframe>');
+// 		}
+//     });   
+// })
+
+// $(function(){
+//     $('#button3').click(function(){ 
+//         if(!$('#iframe').length) {
+// 			//var replaced = $('#iframeHolder').html().replace('<iframe id = "#myiFrame" width="100%" height="800" frameborder="0" allow="xr-spatial-tracking; gyroscope; accelerometer" allowfullscreen scrolling="no" src="https://kuula.co/share/collection/7XlH7?logo=1&info=0&fs=1&vr=1&zoom=1&initload=0&autorotate=0.1&thumbs=1"></iframe>,<iframe id = "#myiFrame" width="100%" height="1000" frameborder="0" allow="xr-spatial-tracking; gyroscope; accelerometer" allowfullscreen scrolling="no" src="https://kuula.co/share/collection/7XlH7?logo=1&info=0&fs=1&vr=1&zoom=1&initload=0&autorotate=0.1&thumbs=1"></iframe>')
+// 			$('#iframeHolder').remove();
+// 			$('#iframeHolder').replaceWith('<iframe id = "#myiFrame" width="100%" height="1000" frameborder="0" allow="xr-spatial-tracking; gyroscope; accelerometer" allowfullscreen scrolling="no" src="https://kuula.co/share/collection/7XlH7?logo=1&info=0&fs=1&vr=1&zoom=1&initload=0&autorotate=0.1&thumbs=1"></iframe>');
+// 		}
+//     });   
+// })
